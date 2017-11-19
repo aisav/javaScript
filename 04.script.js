18a594693839ab8dc1a40946ed2cd46633155c53
arturisav <artur.isaverdyan@gmail.com>
2017-11-19 20:24:26 +0400

    debuging

--- 04.script.js
+++ 04.script.js
@@ -1,2 +1,6 @@
-document.write('From Script file')
-alert('hello 3')
+document.write('From Script file');
+alert('hello 3');
+console.log("message for debug");
+console.log("%cmessage for debug with CSS", "color:green;background:black");
+console.error("errorrrr");
+console.warn("warnnnnnn");
